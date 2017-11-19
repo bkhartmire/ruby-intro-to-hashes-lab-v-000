92b17b6ddb95fda314b2f42bf27337c137da103c
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-19 03:20:33 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,15 +8,18 @@ end
 
 def monopoly
 	monopoly = {}
-	monopoly[:railroads] = {}
+	monopoly = {railroads: {}}
 end
 
 def monopoly_with_second_tier
 	monopoly = {}
-	monopoly[:railroads] = {}
-	monopoly[:railroads][:pieces] = 4
-	monopoly[:railroads][:names] = {}
-	monopoly[:railroads][:rent_in_dollars] = {}
+	monopoly = {railroads: 
+		{piece: 4, 
+			names: {}, 
+			rent_in_dollars: {}
+		}
+	}
+	
 end
 
 def monopoly_with_third_tier
